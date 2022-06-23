1e774ceeca2676c7bcbdb1389f7712c64f43684f
Samuel Sampaio <samueledshotmailgm@gmail.com>
2022-06-23 09:49:04 -0300

    Configurando lógica e parâmetros do Panel

--- src/Dependecies/GamePanel.java
+++ src/Dependecies/GamePanel.java
@@ -1,18 +1,79 @@
 package Dependecies;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
     
+    // Configurações de janela.
+    static final int SCREEN_WIDHT = 600;
+    static final int SCREEN_HEIGHT = 600;
+    
+    // Cálculo para tamanho dos objetos em cena.
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDHT * SCREEN_HEIGHT) / UNIT_SIZE;
+    
+    // Atrasos.
+    static final int DELAY = 75;
+    
+    // CÁLCULO PARA O CORPO DA COBRA.
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    
+    // Variáveis para configuração da lógica do jogo.
+    int bodyParts = 6;
+    int applesEaten = 0;
+    int appleX = 0;
+    int appleY = 0;
+    
+    // Direção (R - Right, U - Up, L - Left, D - Down)
+    char direction = 'R';
+    
     public GamePanel(){
+        
+    }
+    
+    public void StartGame(){
+        
+    }
+    
+    public void paintComonent(Graphics g){
+        
+    }
+    
+    public void draw(Graphics g){
+        
+    }
+    
+    public void move(){
+        
+    }
+    
+    public void checkApple(){
+        
+    }
+    
+    public void checkCollisions(){
+        
+    }
+    
+    public void gameOver(Graphics g){
         
     }
     
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+    
+    public class MyAdapter extends KeyAdapter{
+        
+        @Override
+        public void keyPressed(KeyEvent evt){
+            
+        }
     }
     
 }
