daec9d759dbd6cd849bd0a9c1c203b660450c1ef
Samuel Sampaio <samueledshotmailgm@gmail.com>
2022-06-23 22:13:22 -0300

    Desenhado a cena.

--- src/Dependecies/GamePanel.java
+++ src/Dependecies/GamePanel.java
@@ -30,20 +30,56 @@ public class GamePanel extends JPanel implements ActionListener {
     
     // Direção (R - Right, U - Up, L - Left, D - Down)
     char direction = 'R';
+    boolean running = false;
     
+    Timer timer;
+    Random random;
+    
+    // Confiiguração ao iniciar com o painel.
     public GamePanel(){
         
+        random = new Random();
+        
+        this.setPreferredSize(new Dimension(SCREEN_WIDHT, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        
+        this.addKeyListener(new MyAdapter());
+        
+        StartGame();
+    
     }
     
     public void StartGame(){
         
-    }
-    
-    public void paintComonent(Graphics g){
+        newApple();
+        running = true;
+        
+        timer = new Timer(DELAY, this);
+        timer.start();
         
     }
     
+    public void paintComponent(Graphics g){
+        
+        super.paintComponent(g);
+        draw(g);
+    
+    }
+    
+    // Desenhar na tela.
     public void draw(Graphics g){
+        
+        for(int i = 0; i < (SCREEN_HEIGHT / UNIT_SIZE); i++){
+            
+            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDHT, i * UNIT_SIZE);
+            
+        }
+        
+    }
+    
+    public void newApple(){
         
     }
     
